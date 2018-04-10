a21090970004e1a01039bd67f2906a034963bbb5
juliocesardasilva <devjuliocesarsilva@gmail.com>
2018-04-10 11:30:15 -0300

    ajustes no splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      statusBar.backgroundColorByHexString('#cccccc');
       splashScreen.hide();
     });
   }
