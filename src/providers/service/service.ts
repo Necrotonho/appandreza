b134fb9e87c6827b3bf6cb0fb72d20d912d92ff4
juliocesardasilva <devjuliocesarsilva@gmail.com>
2023-03-23 12:54:43 -0300

    up

--- src/providers/service/service.ts
+++ src/providers/service/service.ts
@@ -25,7 +25,7 @@ export class ServiceProvider {
 
   // private localIp = 'ws://192.168.1.32:8000';
   private externalIP = 'ws://52.2.33.193:8000';
-  
+
   private tentativasIP = [];
   private timeOutRequest = 15000;
   public appFocus: boolean;
@@ -152,7 +152,7 @@ export class ServiceProvider {
 
         content: 'Conectando'
       })
-      loading.present();
+      // loading.present();
       this.connect()
         .then(res => {
 
